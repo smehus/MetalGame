26d3d5b77d814b76d9cbb89c3d869d29293ef0ed
Scott <mehus.scott@gmail.com>
2017-07-18 08:19:15 -0400

    Project files

---
+++ MetalGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalGame
+//
+//  Created by scott mehus on 7/18/17.
+//  Copyright © 2017 scott mehus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
