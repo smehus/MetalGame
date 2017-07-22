8a344d536e5ce40d81f75cfdb19450b68b31e348
Scott <mehus.scott@gmail.com>
2017-07-22 09:05:46 -0400

    Kinda working smooth hills, but im moving on

--- MetalGame/RandomLines.swift
+++ MetalGame/RandomLines.swift
@@ -34,16 +34,54 @@ class RandomLines: Node, Renderable, DefaultVertexDescriptorProtocol {
     
     private func buildVertices() {
         
-        for i in stride(from: -1, through: 1, by: 0.1) {
+        var hillPoints: [Vertex] = []
+        var smoothPoints: [Vertex] = []
+
+        var r = Float.random()
+        var g = Float.random()
+        var b = Float.random()
+        
+        for i in stride(from: -1, through: 1, by: 0.2) {
             let point = Float(i)
-            let r = Float.random()
-            let g = Float.random()
-            let b = Float.random()
+            r = Float.random()
+            g = Float.random()
+            b = Float.random()
+            
             let vert = Vertex(position: float3(point, r, 0), color: float4(r, g, b, 1))
             let vert2 = Vertex(position: float3(point, -1, 0), color: float4(r, g, b, 1))
-            vertices.append(vert)
-            vertices.append(vert2)
+            hillPoints.append(vert)
+            hillPoints.append(vert2)
         }
+        
+        
+        // Trying to draw smooth hills.... and failing
+        for i in 0..<hillPoints.count {
+            guard hillPoints.indices.contains(i + 1) else {
+                print("INDEX: \(i), hillpoints: \(hillPoints.count)")
+                continue
+            }
+            
+            let p0 = hillPoints[i]
+            let p1 = hillPoints[i + 1]
+            
+            let da: Float = Float(Double.pi / Double((hillPoints.count - 1)))
+            let ymid: Float = (p0.position.y + p1.position.y) / 2
+            let ampl: Float = (p0.position.y - p1.position.y) / 2
+            
+            for j in 0..<hillPoints.count {
+                let alpha: Float = Float(j) / Float(hillPoints.count - 1)
+                let x: Float = ((1 - alpha) * p0.position.x) + (alpha * p1.position.x)
+                let y: Float = ymid + ampl * cosf(da * Float(j))
+                
+                let smoothVertex = Vertex(position: float3(x, y, 0), color: float4(1, 0, 0, 1))
+                let bottomSmooth = Vertex(position: float3(x, -1, 0), color: float4(1, 0, 0, 1))
+                smoothPoints.append(contentsOf: [smoothVertex, bottomSmooth])
+            }
+        }
+        
+        
+        
+        vertices = smoothPoints
     }
     
     func performRender(with commandEncoder: MTLRenderCommandEncoder) {
