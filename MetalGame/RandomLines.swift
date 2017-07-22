0226d146e0f014b3c0f2c33b9bb3397870733a80
Scott <mehus.scott@gmail.com>
2017-07-22 13:42:46 -0400

    Works

--- MetalGame/RandomLines.swift
+++ MetalGame/RandomLines.swift
@@ -14,6 +14,10 @@ extension Float {
         let floatValue = Float(arc4random()) / Float(UInt32.max)
         return  arc4random_uniform(2) == 0 ? floatValue : -floatValue
     }
+    
+    static func randomFloatRange(min: Float, max: Float) -> Float {
+        return Float(Float(arc4random()) / Float(UInt32.max)) * (max - min) + min
+    }
 }
 
 class RandomLines: Node, Renderable, DefaultVertexDescriptorProtocol {
@@ -34,55 +38,68 @@ class RandomLines: Node, Renderable, DefaultVertexDescriptorProtocol {
     
     private func buildVertices() {
         
-        var hillPoints: [Vertex] = []
-        var smoothPoints: [Vertex] = []
-
-        var r = Float.random()
-        var g = Float.random()
-        var b = Float.random()
+        let kNumHillKeyPoints = 5
+        let kNumHillSmoothSegments = 100
+        let kNumHillSmoothPoints = (kNumHillKeyPoints - 1) * kNumHillSmoothSegments
+        let kNumHillVertices = kNumHillSmoothPoints * 2
         
-        for i in stride(from: -1, through: 1, by: 0.4) {
-            let point = Float(i)
-            r = Float.random()
-            g = Float.random()
-            b = Float.random()
+        var hillKeyPoints: [Vertex] = Vertex.defaultValues(with: kNumHillKeyPoints)
+        var hillSmoothPoints: [Vertex] = Vertex.defaultValues(with: kNumHillSmoothPoints)
+        var hillVertices: [Vertex] = Vertex.defaultValues(with: kNumHillVertices)
+        
+        let startingY = Float.randomFloatRange(min: -1, max: 1)
+        let firstVertex = Vertex(position: float3(-1, startingY, 0), color: float4(1, 0, 0, 1))
+        hillKeyPoints[0] = firstVertex
+      
+
+        var sign: Float = 1
+        var y: Float  = startingY
+        var x: Float = -1
+        let width: Float = 2
+        
+        for i in 1..<kNumHillKeyPoints {
+            x += width / Float(kNumHillKeyPoints - 1)
+            y = sign * Float.randomFloatRange(min: 0, max: 0.8)
+            sign = -sign
             
-            let vert = Vertex(position: float3(point, r, 0), color: float4(r, g, b, 1))
-            let vert2 = Vertex(position: float3(point, -1, 0), color: float4(r, g, b, 1))
-            hillPoints.append(vert)
-            hillPoints.append(vert2)
+            let vert = Vertex(position: float3(x, y, 0), color: float4(1, 0, 0, 1))
+            hillKeyPoints[Int(i)] = vert
         }
         
+        hillKeyPoints[kNumHillKeyPoints - 1] = Vertex(position: float3(1, startingY, 0), color: float4(1, 0, 0, 1))
         
         // Trying to draw smooth hills.... and failing
-        for i in 0..<hillPoints.count {
-            guard hillPoints.indices.contains(i + 1) else {
-                print("INDEX: \(i), hillpoints: \(hillPoints.count)")
-                continue
-            }
+        for i in 0..<kNumHillKeyPoints {
+            guard hillKeyPoints.indices.contains(i + 1) else { continue }
+            let p0 = hillKeyPoints[i]
+            let p1 = hillKeyPoints[i + 1]
             
-            let p0 = hillPoints[i]
-            let p1 = hillPoints[i + 1]
-            
-            let da: Float = Float(Double.pi / Double((hillPoints.count - 1)))
+            let da: Float = Float(Double.pi / Double((kNumHillSmoothSegments - 1)))
             let ymid: Float = (p0.position.y + p1.position.y) / 2
             let ampl: Float = (p0.position.y - p1.position.y) / 2
             
-            for j in 0..<hillPoints.count {
-                let alpha: Float = Float(j) / Float(hillPoints.count - 1)
+            let r = Float.random()
+            let g = Float.random()
+            let b = Float.random()
+            
+            for j in 0..<kNumHillSmoothSegments {
+                let alpha: Float = Float(j) / Float(kNumHillSmoothSegments - 1)
                 let x: Float = ((1 - alpha) * p0.position.x) + (alpha * p1.position.x)
                 let y: Float = ymid + ampl * cosf(da * Float(j))
                 
-                let smoothVertex = Vertex(position: float3(x, y, 0), color: float4(1, 0, 0, 1))
-                let bottomSmooth = Vertex(position: float3(x, -1, 0), color: float4(1, 0, 0, 1))
-                smoothPoints.append(contentsOf: [smoothVertex, bottomSmooth])
+                let smoothVertex = Vertex(position: float3(x, y, 0), color: float4(r, g, b, 1))
+                hillSmoothPoints[(i * kNumHillSmoothSegments) + j] = smoothVertex
                 
             }
         }
         
+        for (index, point) in hillSmoothPoints.enumerated() {
+            hillVertices[index * 2] = point
+            hillVertices[(index * 2) + 1] = Vertex(position: float3(point.position.x, -1, 0), color: point.color)
+        }
         
         
-        vertices = smoothPoints
+        vertices = hillVertices
     }
     
     func performRender(with commandEncoder: MTLRenderCommandEncoder) {
@@ -99,4 +116,33 @@ class RandomLines: Node, Renderable, DefaultVertexDescriptorProtocol {
     }
 }
 
+extension Vertex: DefaultValuable {
+    static func defaultValue() -> Vertex {
+        return Vertex(position: float3(0, 0, 0), color: float4(0, 0, 0, 1))
+    }
+    
+    static func defaultValues(with num: Int) -> [Vertex] {
+        var values: [Vertex] = []
+        for _ in 0..<num {
+            values.append(Vertex.defaultValue())
+        }
+        
+        return values
+    }
+}
 
+protocol DefaultValuable {
+    static func defaultValue() -> Self
+}
+
+extension Array {
+    static func DefaultValues<T: DefaultValuable>(num: Int) -> [T] {
+        var what: [T] = []
+        for _ in 0..<num {
+            let v = T.defaultValue()
+            what.append(v)
+        }
+        
+        return what
+    }
+}
